b46760292aa6c309d9c3b88951001aafdc9aa922
NikiforDragov <ndragov99@gmail.com>
2025-10-26 12:40:52 +0200

    Add weather card component and display flex to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import WeatherCard from './components/WeatherCard';
 import './css/App.css';
 
 function App() {
-    return <main className='main-content'></main>;
+    return (
+        <main className='main-content'>
+            <WeatherCard />
+        </main>
+    );
 }
 
 export default App;
---
+++ src/components/WeatherCard.jsx
@@ -0,0 +1,11 @@
+function WeatherCard() {
+    return <div className="weather-card">
+        <p className="weekday">Tuesday</p>
+        <p className="date">20 Jun 2022</p>
+        <p className="location">Plovdiv, BG</p>
+        <p className="temperature">29 C</p>
+        <p className="weather-type">Sunny</p>
+    </div>
+}
+
+export default WeatherCard;
